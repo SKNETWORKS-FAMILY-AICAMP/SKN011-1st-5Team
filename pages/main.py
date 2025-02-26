18cf5d4e8d365d91a8b1d88747c3f7c03434025e
yugyeongh <uugyeong27@gmail.com>
2025-02-26 10:43:01 +0900

    edit main.py

--- pages/main.py
+++ pages/main.py
@@ -14,7 +14,7 @@ page = st.sidebar.radio(
 
 
 def run_selected_page(page_name):
-    if page_name == "🏠 Home":
+    if page_name == "Home":
         home.run()
     elif page_name == "💡 국산 신차 판매 현황":
         korean_cars.run()
